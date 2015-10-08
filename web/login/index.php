7a4a163cb86db280ed3aa7865b6fe4a10e714cb6
Nate Krofft <nate437@gmail.com>
2015-10-07 22:27:13 -0500

    testing auth + playlist filling...

--- web/login/index.php
+++ web/login/index.php
@@ -1,6 +1,40 @@
 <?php
 require('../../conf.php');
-//echo "code: " . $_GET["code"];
-header('Location: ../?token=' . $_GET["code"]);
+
+$url = "https://accounts.spotify.com/api/token";
+
+$token = $_GET["token"]
+
+$fields = array(
+  'grant_type' => 'authorization_code',
+  'code' => $_GET["code"],
+  'client_id' => $client_id,
+  'client_secret' => $client_secret,
+  'redirect_uri' => 'http://laminarnq.com/login'
+);
+
+$fields_string = "";
+
+foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+rtrim($fields_string, '&');
+
+//open connection
+$ch = curl_init();
+
+//set the url, number of POST vars, POST data
+curl_setopt($ch,CURLOPT_URL, $url);
+curl_setopt($ch,CURLOPT_POST, count($fields));
+curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+
+//execute post
+$result = curl_exec($ch);
+echo $result;
+
+$resObj = json_decode($result);
+
+header('Location: ../?code=' . $resObj.access_token);
+
+//close connection
+curl_close($ch);
 
 ?>
