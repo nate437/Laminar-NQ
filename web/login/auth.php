7c492eff721820d46d5db5d1133b13a7e52e0ba1
Laminar <laminar@nate>
2015-10-07 21:38:02 -0500

    fixing stupids

--- web/login/auth.php
+++ web/login/auth.php
@@ -1,11 +1,11 @@
 <?php
-require('../conf.php');
+require('../../conf.php');
 
 $fields = array(
   'grant_type' => 'authorization_code',
   'code' => $_get["token"],
   'redirect_uri' => 'http://laminarnq.com/login'
-)
+);
 
 $resp =  http_post_fields("https://accounts.spotify.com/api/token", $fields);
 
