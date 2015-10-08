306a1a6b65aff4521dfbffa4b8e7a4b97ca92dbc
Nate Krofft <nate437@gmail.com>
2015-10-07 21:36:27 -0500

    fixing stupids

--- web/login/auth.php
+++ web/login/auth.php
@@ -1,13 +1,21 @@
 <?php
 require('../conf.php');
-
-$fields = array(
+$url = "https://accounts.spotify.com/api/token";
+$data = array(
   'grant_type' => 'authorization_code',
   'code' => $_get["token"],
   'redirect_uri' => 'http://laminarnq.com/login'
 )
 
-$resp =  http_post_fields("https://accounts.spotify.com/api/token", $fields);
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data),
+    ),
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
 
-echo $resp;
+var_dump($result);
 ?>
